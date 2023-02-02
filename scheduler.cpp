b130fe4f28a2bfd5658d309664cfba693f32385a
Jaden Creech <jcreech1@cs.binghamton.edu>
2023-02-02 17:52:00 -0500

    Added prompt function

--- scheduler.cpp
+++ scheduler.cpp
@@ -3,11 +3,24 @@
 #include<stdlib.h>
 #include<vector>
 #include<sstream>
+using namespace std;
+
 
 int main() {
   
-
-
-
   return 0;
+}
+
+
+void showPrompt() {
+
+            cout << "Enter [\"build <crn> <department> <number> <name>\"" << std::endl <<
+                  "       \"cancel <crn>\"" << std::endl <<
+                  "       \"enroll <bnumber> <userid> <first> <last>\"" << std::endl <<
+                  "       \"add <bnumber> <crn>\"" << std::endl <<
+                  "       \"drop <bnumber> <crn>\"" << std::endl <<
+                  "       \"roster <crn>\"" << std::endl <<
+                  "       \"schedule <bnumber>\"" << std::endl <<
+                  "       \"quit\"]" << endl <<
+                  ": ";
 }
